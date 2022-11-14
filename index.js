0aa81c874e7f2870bc2e6c577a5ac1f29d6a3da0
Mehdi van Loon <mehdivanloon123@gmail.com>
2022-11-14 20:21:14 +0100

    practice

---
+++ index.js
@@ -0,0 +1,66 @@
+import rl from "readline-sync";
+
+const number = 5;
+// take range input from the user
+const range = 5;
+
+//creating a multiplication table
+for (let i = 1; i <= range; i++) {
+  const result = i * number;
+  console.log(`${number} * ${i} = ${result}`);
+}
+
+/*************************************************************/
+
+const numbers = [1, 2, 3, 4, 5, 6];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+  console.log(`de som is ${sum}`);
+}
+const voorNaam = ["David", "Els", "Mo", "Bert", "Fatima", "Gunther"];
+const achterNaam = [
+  "Verhulst",
+  "Vermeiren",
+  "Michiels",
+  "Legrand",
+  "Errazzouki",
+  "Gunther",
+];
+
+if (voorNaam.length != achterNaam.length) {
+  console.log("we miss a name");
+} else {
+  console.log("perfect!");
+}
+
+for (let i = 0; i < voorNaam.length; i++) {
+  console.log(`${voorNaam[i]} ${achterNaam[i]}`);
+}
+/******************************************************************** */
+
+/*
+let vriendenLijst = [];
+
+const askHoeveel = rl.question("Hoeveel vrienden wens je in te geven?");
+console.log(askHoeveel);
+for (let i = 0; i < askHoeveel; i++) {
+  const askPerKeer = rl.question(`Geef vriend ${i + 1} in?`);
+  console.log(askPerKeer);
+  vriendenLijst.push(askPerKeer);
+}
+/*console.log(`${vriendenLijst}     // print de lijst niet onder elkaar uit
+`); */
+/*
+for (let i = 0; i < vriendenLijst.length; i++) { // hierdoor print de lijst wel onder elkaar uit 
+  console.log(`-${vriendenLijst[i]}\n`);
+}
+*/
+/************************************************************************************ */
+
+const teRadenGetal = Math.floor(Math.random()*30)
+
+const geradenGetal = rl.question("Geef een getal tussen 0 en 30 aub?")
+
+
